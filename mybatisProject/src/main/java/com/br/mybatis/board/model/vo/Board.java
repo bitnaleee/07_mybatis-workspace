917e84da9dfbcff8bd681a40c3a77e024d11f8ab
bitna_lee <lbn063017@naver.com>
2023-05-30 15:27:33 +0900

    마이바티스 세팅

---
+++ mybatisProject/src/main/java/com/br/mybatis/board/model/vo/Board.java
@@ -0,0 +1,105 @@
+package com.br.mybatis.board.model.vo;
+
+import java.sql.Date;
+
+import com.br.mybatis.member.model.vo.Member;
+
+public class Board {
+
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardWriter;
+	private int count;
+	private Date createDate;
+	private String status;
+	
+	private Member mem; // 1대1관계 (한 게시글에 한 작성자이기 때문)
+	
+	public Board() {}
+
+	public Board(int boardNo, String boardTitle, String boardContent, String boardWriter, int count, Date createDate,
+			String status) {
+		super();
+		this.boardNo = boardNo;
+		this.boardTitle = boardTitle;
+		this.boardContent = boardContent;
+		this.boardWriter = boardWriter;
+		this.count = count;
+		this.createDate = createDate;
+		this.status = status;
+	}
+
+	public int getBoardNo() {
+		return boardNo;
+	}
+
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+
+	public String getBoardContent() {
+		return boardContent;
+	}
+
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+
+	public String getBoardWriter() {
+		return boardWriter;
+	}
+
+	public void setBoardWriter(String boardWriter) {
+		this.boardWriter = boardWriter;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Member getMem() {
+		return mem;
+	}
+
+	public void setMem(Member mem) {
+		this.mem = mem;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [boardNo=" + boardNo + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
+				+ ", boardWriter=" + boardWriter + ", count=" + count + ", createDate=" + createDate + ", status="
+				+ status + "]";
+	}
+	
+}
+
